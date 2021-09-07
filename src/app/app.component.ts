64ffc6ac562dffa257caeb1dab6250002fabd969
hannah <hannahnajwa@gmail.com>
2021-09-07 20:14:28 +0800

    working on animate invoice modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { LayoutStateService } from './services/layout-state.service';
 import { Component, HostListener } from '@angular/core';
 import { Observable } from 'rxjs';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('insertRemoveTrigger', [
+      state('*', style({ zIndex: '10' })),
+      transition(':enter', [
+        style({ transform: 'translateX(-700px)' }),
+        animate('500ms ease-in'),
+      ]),
+      transition(':leave', [
+        animate(
+          '500ms ease',
+          style({ transform: 'translateX(-700px)', overflowY: 'hidden' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   mobile: any = null;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvoiceModalComponent } from './components/invoice-modal/invoice-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavigationComponent, InvoiceModalComponent],
-  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
