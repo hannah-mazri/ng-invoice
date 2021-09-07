d1659860ff007ffd931c473d28a1e3e2b2d80980
hannah <hannahnajwa@gmail.com>
2021-09-07 16:09:23 +0800

    add home header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  filterMenu = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  newInvoice() {}
+
+  toggleFilterMenu() {
+    this.filterMenu = !this.filterMenu;
   }
-
 }
