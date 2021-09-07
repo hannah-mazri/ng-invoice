233b2f53713316a4a9a3ca1f1a7fac342110a791
hannah <hannahnajwa@gmail.com>
2021-09-07 15:41:42 +0800

    import navigation and add mobile detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-invoice';
+  mobile: any = null;
+
+  @HostListener('window:resize', [])
+  private onResize() {
+    this.detectScreenSize();
+  }
+
+  private detectScreenSize() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth <= 750) {
+      this.mobile = true;
+      return;
+    }
+    this.mobile = false;
+  }
 }
