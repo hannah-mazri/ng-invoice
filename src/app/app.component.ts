ae665e260359cf20523fb9202420778168999ef8
hannah <hannahnajwa@gmail.com>
2021-09-09 23:46:12 +0800

    use angular animation to animate invoice modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,24 @@ import { LayoutStateService } from './services/layout-state.service';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { InvoiceStateService } from './services/invoice-state.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('insertRemoveTrigger', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('800ms ease', style({ transform: 'translateX(-110px)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(-110px)' }),
+        animate('800ms ease', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   mobile: any = null;
